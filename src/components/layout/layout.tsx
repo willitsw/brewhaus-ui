63d7ac5b8d8cda6891d42315da7f948e7cb2fb32
William Willits <willitsw@gmail.com>
2023-01-04 06:56:58 -0500

    redirect to home

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "./header";
 import { Divider, Layout as AntDLayout } from "antd";
 import Footer from "./footer";
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/home");
+    }
+  }, [location.pathname]);
+
   return (
     <AntDLayout>
       <Header />
